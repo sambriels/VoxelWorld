d98ef635118f2a581f8b5a5cb200aae013b037a8
Sam Briels <sambriels@gmail.com>
2022-02-23 08:55:55 +0100

    Initial commit with UE5.0 preview

---
+++ Source/VoxelWorld/Private/VWChunk.cpp
@@ -0,0 +1,14 @@
+#include "VoxelWorld/Public/VWChunk.h"
+
+AVWChunk::AVWChunk()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AVWChunk::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/VoxelWorld/Public/VWChunk.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "VWChunk.generated.h"
+
+enum class EBlock;
+enum class EDirection;
+class FastNoiseLite;
+class UProceduralMeshComponent;
+
+UCLASS()
+class AVWChunk : public AActor
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this actor's properties
+	AVWChunk();
+
+	UPROPERTY(EditAnywhere, Category="Chunk")
+	int Size = 32;
+
+	UPROPERTY(EditAnywhere, Category="Chunk")
+	int Scale = 1;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void GenerateBlocks();
+	void GenerateMesh();
+	void ApplyMesh() const;
+	bool Check(FVector Position) const;
+	void CreateFace(EDirection Direction, FVector Position);
+	TArray<FVector> GetFaceVertices(EDirection Direction, FVector Position) const;
+	FVector GetPositionInDirection(EDirection Direction, FVector Position) const;
+	int GetBlockIndex(int X, int Y, int Z) const;
+	
+	TObjectPtr<UProceduralMeshComponent> Mesh;
+	TObjectPtr<FastNoiseLite> Noise;
+
+	TArray<EBlock> Blocks;
+
+	TArray<FVector> VertexData;
+	TArray<int> TriangleData;
+	TArray<FVector2D> UVData;
+
+	int VertexCount = 0;
+
+	const FVector BlockVertexData[8] = {
+		FVector(100,100,100),
+		FVector(100,0,100),
+		FVector(100,0,0),
+		FVector(100,100,0),
+		FVector(0,0,100),
+		FVector(0,100,100),
+		FVector(0,100,0),
+		FVector(0,0,0)
+	};
+
+	const int BlockTriangleData[24] = {
+		0,1,2,3, // Forward
+		5,0,3,6, // Right
+		4,5,6,7, // Back
+		1,4,7,2, // Left
+		5,4,1,0, // Up
+		3,2,7,6  // Down
+	};
+};
